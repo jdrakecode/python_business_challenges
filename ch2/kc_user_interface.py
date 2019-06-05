f5d1dfc7f3920e07957532e5667ac5b6e326685e
jdrakecode <jdrakepaypal@gmail.com>
2019-06-04 23:52:11 -0400

    mvp

--- ch2/kc_user_interface.py
+++ ch2/kc_user_interface.py
@@ -23,22 +23,46 @@ Choose a menu item:
         )
     
     def see_all(self):
-        print("ClaimID  |  Type  |  Amount  |  DateOfAccident  |  DateOfClaim  |  IsValid  |  Description")
         cl = self.claim_repo.claim_list
-        for i in cl:
-            print(i)
+        if cl != []:
+            print("\nClaimID  |  Type  |  Amount  |  DateOfAccident  |  DateOfClaim  |  IsValid  |  Description")
+            cl = self.claim_repo.claim_list
+            for i in cl:
+                print(i)
+        else:
+            print("\nFortunately, claims list is empty!")
 
     def next_claim(self):
-        pass
+        while True:
+            cl = self.claim_repo.claim_list
+            if cl != []:
+                print("\nClaimID  |  Type  |  Amount  |  DateOfAccident  |  DateOfClaim  |  IsValid  |  Description\n", cl[0])
+                yorn = input("\nDo you want to deal with this claim now (y/n)?\n> ")
+                if yorn == "y":
+                    print("\nClaim taken.")
+                    del cl[0]
+                    if cl != []:
+                        print("\nNext claim:")
+                        continue
+                    else:
+                        continue
+                elif yorn == "n":
+                    menu.run()
+                else:
+                    print("\nPlease enter y for Yes or n for No")
+                    continue
+            else:
+                print("\nClaims list is empty!")
+                break
 
     def new_claim(self):
-        claim_id = input("Enter claim id:\n ")
-        claim_type = input("Enter claim type:\n ")
-        amount = input("Amount of Damage:\n ")
-        date_accident = input("Date of Accident:\n ")
-        date_claim = input("Date of Claim:\n ")
-        is_valid = input("Claim made within 30 days?\nEnter True or False:\n ")
-        description = input("Enter a claim description:\n ")
+        claim_id = input("Enter claim id:\n> ") # Stretch Goal: assign by increments of 1 instead of input
+        claim_type = input("Enter claim type:\n> ")
+        amount = input("Amount of Damage:\n> ")
+        date_accident = input("Date of Accident:\n> ")
+        date_claim = input("Date of Claim:\n> ")
+        is_valid = input("Claim made within 30 days?\nEnter True or False:\n> ") # Stretch Goal: change to check for t/n and y/n also
+        description = input("Enter a claim description:\n> ")
         self.claim_repo.create_claim(claim_id, claim_type, amount, date_accident, date_claim, is_valid, description)
         print(f"Claim number {claim_id} has been added")
 
--- ch2/komodo_claims.py
+++ ch2/komodo_claims.py
@@ -1,5 +1,5 @@
 # Komodo Claims
-
+#  -> git fetch -> remove tomorrow morning
 class Claim:
     def __init__(self, claim_id, claim_type, amount, date_accident, date_claim, is_valid, description):
         self.claim_id = claim_id
@@ -12,7 +12,7 @@ class Claim:
     
     def __repr__(self):
         return f"{self.claim_id}           {self.claim_type}      {self.amount}         {self.date_accident}            {self.date_claim}         {self.is_valid}        {self.description}"
-        # Format
+        # Formats strangely when user inputs are too long or too short
         # ClaimID   Type    Amount  DateOfAccident  DateOfClaim IsValid Description
         # 1         Car     $400    4-25-19         4-27-19     True    Fender Bender     
 
@@ -24,3 +24,5 @@ class ClaimRepository:
         new_claim = Claim(claim_id, claim_type, amount, date_accident, date_claim, is_valid, description)
         self.claim_list.append(new_claim)
     
+    def process_claim(self):
+        pass
